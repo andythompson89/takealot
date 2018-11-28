00452e90105cda2cc8d03586947ab9329f5de30f
Mark Berndt <berndtmark@gmail.com>
2018-11-28 21:09:18 +0000

    published

--- TakealotApi/Function1.cs
+++ TakealotApi/Function1.cs
@@ -2,18 +2,20 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace TakealotApi
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */45 * * * *")]TimerInfo myTimer, ILogger log)
         {
             var apiClient = new ApiClient("https://api.takealot.com/rest/v-1-6-0/productline");
             var productResult = apiClient.GET<object>("PLID49432854");
 
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            string jsonResult = JsonConvert.SerializeObject(productResult);
+            log.LogInformation($"Timer trigger function executed: {jsonResult}");
         }
     }
 }
